02460e5960cc92b5cbe72f9245d20114602dedf0
sahinturk <kopuskopecik@gmail.com>
2020-06-26 15:33:28 +0300

    Start project

--- sofistik/settings.py
+++ sofistik/settings.py
@@ -24,16 +24,7 @@ CKEDITOR_CONFIGS = {
 	},
 }
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
 
 from django.contrib.messages import constants as messages
 
